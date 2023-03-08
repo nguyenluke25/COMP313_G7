e257bfc574265ccd8ba2fe14b43df77924cb3033
nguyenluke25 <71531230+nguyenluke25@users.noreply.github.com>
2023-03-07 20:07:15 -0500

    display results of filtered search (#9)
    display filtered search results

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,114 +1,146 @@
 import { useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Alert, Card, Col, Row } from "react-bootstrap";
+import { Form, LoaderFunction, json, useLoaderData } from "remix";
+import AdvancedSearch from "~/components/advanced-search";
+import RecipeList, { Recipe, recipeForClient } from "~/components/recipe-list";
+import { parseToInt } from "~/utils/parseString";
+import {
+  getRandomRecipes,
+  searchRecipesByTitle,
+} from "~/utils/spoonacular.server";
 
-export default function AdvancedSearch(): JSX.Element {
-  const [included, setIncluded] = useState("");
-  const [excluded, setExcluded] = useState("");
+/**
+ * The data returned by the loader for the main page (when not in search mode)
+ */
+interface MainData {
+  type: "main";
+  recipes: Recipe[];
+}
+/**
+ * The data returned by the loader in case of any input validation errors
+ */
+interface ErrorData {
+  status: "error";
+  message: string;
+}
+/**
+ * The data returned by the loader in case of any input validation errors
+ */
 
-  const addIncluded = (event) => {
-    event.preventDefault();
-    console.log(included);
-    console.log("include ingredient clicked");
-  };
+/**
+ * The data returned by the loader in case of any input validation errors
+ */
 
-  const removeIncluded = (event) => {
-    event.preventDefault();
-    console.log("include remove ingredient clicked");
-  };
+/**
+ * The data returned by the loader for searching by title
+ */
+interface SearchData {
+  type: "search";
+  recipes: Recipe[];
+  currentPage: number;
+  totalPages: number;
+}
 
-  const addExcluded = (event) => {
-    event.preventDefault();
-    console.log("exclude ingredient clicked");
-  };
+/**
+ * The aggregated data type that the loader returns
+ */
+type LoaderData = MainData | SearchData;
 
-  const removeExcluded = (event) => {
-    event.preventDefault();
-    console.log("exclude remove ingredient clicked");
-  };
+/**
+ * Server-side function to load the data needed for the `/` index route
+ *
+ * We need to get the list of available recipes
+ */
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
 
-  const savedFilters = (event) => {
-    event.preventDefault();
-    console.log("saved filters clicked");
-  };
+  // The search query provided by the user
+  const search = url.searchParams.get("q")!;
 
+  // The search query must  have 3 letters at least
+  const ThreeCharRule = /^[a-zA-Z]{3,}$/;
+
+  //Checking if the query has 3 letters or not
+  if (ThreeCharRule.test(search) === false) {
+    return alert("The search query must  have 3 letters at least");
+  }
+  // The page to show, starting from 0
+  const page = parseToInt(url.searchParams.get("p") ?? "");
+
+  // If the search is empty, we return the random list of recipes
+  if (!search) {
+    const { recipes } = await getRandomRecipes({ number: 9 });
+    return json<MainData>({
+      type: "main",
+      recipes: recipes.map(recipeForClient),
+    });
+  }
+
+  const NUM_RESULTS_PER_PAGE = 9;
+  const results = await searchRecipesByTitle(search, {
+    number: NUM_RESULTS_PER_PAGE,
+    offset: page * NUM_RESULTS_PER_PAGE,
+  });
+  const totalPages = Math.ceil(results.totalResults / NUM_RESULTS_PER_PAGE);
+  const currentPage = results.offset / NUM_RESULTS_PER_PAGE;
+  return json<SearchData>({
+    type: "search",
+    totalPages,
+    currentPage,
+    recipes: results.results.map(recipeForClient),
+  });
+};
+
+/**
+ * React component for UI for this page
+ */
+export default function Index() {
+  const data = useLoaderData<LoaderData>();
+  let recipes: Recipe[] = data.recipes;
+
+  // Form is not called from component due to issues with the form not
+  // submitting correctly as a component
   return (
-    <div>
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <button
-          className="advanced-search"
-          style={{ width: "8%" }}
-          onClick={savedFilters}
-        >
-          Saved filters
-        </button>
-      </div>
-      <div className="tools">
-        <h3>Ingredients</h3>
-        <div className="ingredients">
-          <div style={{ display: "flex", marginBottom: "1%" }}>
-            <input
-              type="text"
-              id="text"
-              name="text"
-              value={included}
-              placeholder="Included Ingredients"
-            />
-            <button className="searchbutton" onClick={addIncluded}>
-              +
-            </button>
-            <button className="searchbutton" onClick={removeIncluded}>
-              -
-            </button>
-          </div>
-          <div style={{ display: "flex", marginBottom: "1%" }}>
-            <input
-              type="text"
-              id="text"
-              name="text"
-              value={excluded}
-              placeholder="Excluded Ingredients"
-            />
-            <button className="searchbutton" onClick={addExcluded}>
-              +
-            </button>
-            <button className="searchbutton" onClick={removeExcluded}>
-              -
-            </button>
-          </div>
+    <>
+      <Form method="get">
+        <div className="container">
+          <SearchByTitle />
         </div>
+      </Form>
+      <div className="container">
+        <RecipeList recipes={recipes} />
       </div>
+    </>
+  );
+}
 
-      <div className="tools">
-        <h3>Tools</h3>
-        <div className="ingredients">
-          <div style={{ display: "flex", marginBottom: "1%" }}>
-            <input
-              type="text"
-              id="text"
-              name="text"
-              placeholder="Cooking Tools"
-            />
-            <button className="searchbutton" onClick={addIncluded}>
-              +
-            </button>
-            <button className="searchbutton" onClick={removeIncluded}>
-              -
-            </button>
-          </div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <label htmlFor="time">Estimated Cooking Time</label>
-            <input
-              type="range"
-              id="time"
-              name="time"
-              min="0"
-              max="100"
-              defaultValue={"0"}
-              style={{ width: "10%" }}
-            />
-          </div>
+function SearchByTitle(): JSX.Element {
+  return (
+    <>
+      <div className="Search-UI">
+        <div className="search">
+          <input
+            type="text"
+            name="q"
+            id="searchQuery"
+            className="search-bar"
+            placeholder="Search for the recipe you want"
+          />
+          <button type="submit" className="searchbutton">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 1024 1024"
+              height="1.5em"
+              width="1.5em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
+            </svg>
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
